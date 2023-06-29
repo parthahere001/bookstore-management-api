3d72fec32ef9cf8f428508da410be9df3078a053
parthahere001 <parthahere001.com>
2023-06-29 16:23:34 +0530

    [ADD] Models

---
+++ pkg/models/book.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/parthahere001/go-bookstore-api/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name string `grom:""json:"name"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+
+}
+
+
+func init (){
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+	
+}

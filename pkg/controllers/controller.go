ae0b1b27df73ce00f6c8afa033d478a54c6da06e
parthahere001 <parthahere001.com>
2023-06-29 17:13:28 +0530

    [ADD] getbookbyid controller

--- pkg/controllers/controller.go
+++ pkg/controllers/controller.go
@@ -22,3 +22,20 @@ func GetBooks(w http.ResponseWriter, r *http.Request){
 	w.Write(res)
 }
 
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err:= strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+	
+
+
+}
